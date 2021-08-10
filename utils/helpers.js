bde524f1a1be45472d931b57a82c3812b0bc8c7d
Payne Kerz <paynekerz@gmail.com>
2021-08-10 03:34:17 -0700

    helper changes

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,5 +1,6 @@
-module.exports = {
-    format_date: date => {
-        return `${date.getMonth() + 1}/${date.getDate()}/${date.setFullYear()}`
-    }
-}
+module.exports= {
+    format_date: (date) => {
+        // Format date as MM/DD/YYYY
+        return date.toLocaleDateString();
+      },  
+};
